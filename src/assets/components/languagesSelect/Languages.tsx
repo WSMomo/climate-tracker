1f81840095b9bdc751df8398a54c1a2afe70e9a4
Momo M <momomarcoj@gmail.com>
2023-08-20 11:25:58 +0200

    improved languages selection style

--- src/assets/components/languagesSelect/Languages.tsx
+++ src/assets/components/languagesSelect/Languages.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
-import i18n from "../../i18n";
-import { RootState } from "../redux/store";
-import { setLanguage } from "../redux/languagesReducer";
+import i18n from "../../../i18n";
+import { RootState } from "../../redux/store";
+import { setLanguage } from "../../redux/languagesReducer";
 
 export default function Languages() {
   const dispatch = useDispatch();
@@ -13,12 +13,13 @@ export default function Languages() {
   }
 
   return (
-    <div className="absolute top-2 right-2">
+    <div className="absolute top-2 right-2 border border-text-color">
       <select
         name="languages"
         id="languages"
         onChange={handleLanguageChange}
         value={language}
+        className="text-text-color bg-secondary-color appearance-none py-1 px-2 cursor-pointer"
       >
         <option value="en">EN</option>
         <option value="it">IT</option>
