0388821708ea8a79b325e5315d44d6f55706ad90
Momo M <momomarcoj@gmail.com>
2023-08-22 04:05:55 +0200

    added refresh button to the loader

--- src/assets/components/appComponents/loader/Loader.tsx
+++ src/assets/components/appComponents/loader/Loader.tsx
@@ -1,11 +1,23 @@
 import styles from "./loader.module.css";
 
 import ClientApi from "../../clientApi/ClientApi";
+import { useTranslation } from "react-i18next";
 
 function Loader() {
+  const { t } = useTranslation("appTranslation");
+
+  function refresh() {
+    return window.location.reload();
+  }
+
   return (
     <ClientApi>
-      <div className={styles.loader} role="status"></div>
+      <div className={styles.container}>
+        <div className={styles.loaderAnimation} role="status"></div>
+        <button onClick={refresh} className={styles.refreshButton}>
+          {t("refreshButton")}
+        </button>
+      </div>
     </ClientApi>
   );
 }
