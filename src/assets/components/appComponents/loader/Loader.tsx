f0d72281676aaaf8c21c625738f15443a00ce1d1
Momo M <momomarcoj@gmail.com>
2023-08-22 04:21:18 +0200

    update loader style

--- src/assets/components/appComponents/loader/Loader.tsx
+++ src/assets/components/appComponents/loader/Loader.tsx
@@ -2,21 +2,34 @@ import styles from "./loader.module.css";
 
 import ClientApi from "../../clientApi/ClientApi";
 import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
 
 function Loader() {
   const { t } = useTranslation("appTranslation");
+  const [showRefreshButton, setShowRefreshButton] = useState(false);
 
   function refresh() {
     return window.location.reload();
   }
 
+  useEffect(() => {
+    const timeourId = setTimeout(() => {
+      setShowRefreshButton(true);
+    }, 5000);
+
+    return () => clearTimeout(timeourId);
+  });
+
   return (
     <ClientApi>
       <div className={styles.container}>
-        <div className={styles.loaderAnimation} role="status"></div>
-        <button onClick={refresh} className={styles.refreshButton}>
-          {t("refreshButton")}
-        </button>
+        {showRefreshButton ? (
+          <button onClick={refresh} className={styles.refreshButton}>
+            {t("refreshButton")}
+          </button>
+        ) : (
+          <div className={styles.loaderAnimation} role="status"></div>
+        )}
       </div>
     </ClientApi>
   );
